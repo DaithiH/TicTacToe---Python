84a216796a476080bd71d315a01b7c27db5499cc
DaithiH <davehowley@gmail.com>
2023-12-12 21:12:58 +0000

    first commit

---
+++ TicTacToe.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 25 13:43:32 2023
+
+@author: daveh
+"""
+
+"""
+Second Attempt 
+"""
+
+print("Welcome to my TicTacToe game!\n"
+      "Take your move by entering the number of the square where you "
+      "want to place your token.\n"
+      "The board is indexed as follows:\n"
+      " 1 | 2 | 3\n"
+      " 4 | 5 | 6\n"
+      " 7 | 8 | 9")
+
+board = [" " for x in range(9)]
+
+
+def game_board():
+    row1 = "{}|{}|{}".format(board[0], board[1], board[2])
+    row2 = "{}|{}|{}".format(board[3], board[4], board[5])
+    row3 = "{}|{}|{}".format(board[6], board[7], board[8])
+
+    border = "-+-+-"
+    
+    print()
+    print(row1)
+    print(border)
+    print(row2)
+    print(border)
+    print(row3)
+    
+def player_turn(token):
+    """
+    Function to allocate player tokens and check for 
+    valid inputs. Checks space on the board and if empty places token.
+    If space is full player is promped again.
+    """
+    
+    if token == 'X':
+        player = 1 
+    elif token == 'O':
+        player = 2
+    
+    print(f"Your move player {player}")
+    
+   
+    #move = input("Place your token (1-9): ").strip()
+    #while move.isdigit():
+    while True:
+        
+        try:
+            # TO HANDLE EXCEPTIONS LIKE INVALID NUMBER 
+            # OR IF PLAYER INPUTS A NON-NUMERIC VALUE
+            # WILL CATCH THE INVALID INPUT AND PROMPT
+            # THE PLAYER AGAIN 
+            move = int(input("Place your token (1-9): ").strip())
+            # .strip() WILL REMOVE ANY WHITESPACE
+            if 1 <= move <=9:
+    
+                if board[move-1] == " ":
+                    board[move-1] = token
+                    break
+    
+                else:
+                    print("That square is full! Try again.")
+                   # print("You must enter a number between 1-9. Try again.")
+                   #move = int(input("Place your token (1-9): ").strip())
+
+                
+        except ValueError:
+            print("You must enter a number between 1-9. Try again.")
+
+                
+            
+            
+def is_winner(token):
+    """
+    Checking board for winning combinations
+    """
+    if (board[0] == token and board[1] == token and board[2] == token) or \
+       (board[3] == token and board[4] == token and board[5] == token) or \
+       (board[6] == token and board[7] == token and board[8] == token) or \
+       (board[0] == token and board[3] == token and board[6] == token) or \
+       (board[1] == token and board[4] == token and board[7] == token) or \
+       (board[2] == token and board[5] == token and board[8] == token) or \
+       (board[0] == token and board[4] == token and board[8] == token) or \
+       (board[2] == token and board[4] == token and board[6] == token):
+           return True
+    else:
+        return False
+    
+def a_tie():
+    if " " not in board:
+        return True
+    else:
+        return False
+    
+    
+                  
+while True:
+    game_board()
+    player_turn('X')
+    
+    game_board()
+    if is_winner("X"):
+        #game_board()
+        print("Player 1 wins!")
+        break
+    elif a_tie():
+        print("The game is tied!")
+        break
+    
+    player_turn('O')
+    
+    #game_board()
+    if is_winner("O"):
+        game_board()
+        print("Player 2 wins!")
+        break
+    elif a_tie():
+        print("The game is tied!")
+        break
+    
+    
+  
+    
+    
+        
+        
+    
+    
+    
+    
+    
+        
+
+
+
---
+++ TicTacToeGUI.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 6 18:40 2023
+
+@author: daveh
+
+Taken from Medium
+https://medium.com/@theethicsgeek/building-a-tic-tac-toe-game-with-python-and-tkinter-a-gui-based-journey-into-classic-fun-f2c96a8966eb
+
+"""
+
+import tkinter as tk
+from tkinter import messagebox
+
+class TicTacToe:
+    def __init__(self):
+        """
+        Create instances of the game window, 
+        board, player, and GUI buttons
+        """
+        self.window = tk.Tk()
+        self.window.title("Tic Tac Toe")
+        self.current_player = "X"
+        self.board = [[" " for _ in range(3)] for _ in range (3)]
+        self.buttons = [[None for _ in range(3)] for _ in range(3)]
+        
+        
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j] = tk.Button(
+                    self.window,
+                    text = " ",
+                    font = ("Segoe Script", 20),
+                    width = 10,
+                    height = 5,
+                    command = lambda row = i, col = j: self.button_click(row,col)
+                    )
+                self.buttons[i][j].grid(row = i, column = j)
+                
+    def run(self):
+        self.window.mainloop()
+                
+               
+    def button_click(self, row, col):
+        """
+        The button click on screen is the event that will trigger
+        all the functions to place token, check for win or draw, swap players,
+        and reset the game
+
+        """
+            
+        for r in self.board: print("before move: ", r)
+
+        if self.board[row][col] == " ":
+            self.board[row][col] = self.current_player
+            self.buttons[row][col]["text"] = self.current_player
+            #print("After move: ", row)
+            #self.change_player()
+            #print(self.current_player())
+        if self.is_winner():
+            self.display_winner()
+            self.game_reset()
+        elif self.is_tie():
+            self.display_tie()
+            self.game_reset()
+        else:
+            self.change_player()
+            
+            
+        for x in self.board: print("after move: ", x)
+        
+        print(self.current_player)
+                    
+        
+    def is_winner(self):
+     # CHECK ROWS
+        for row in self.board:
+           if row[0] == row[1] == row[2] != " ":
+               return True
+        
+# CHECK COLUMNS
+        for col in range(3):
+            if self.board[0][col] == self.board[1][col] == self.board[2][col] != " ":
+                return True
+            
+# CHECK DIAGONALS
+        if(self.board[0][0] == self.board[1][1] == self.board[2][2] != " " or  
+           self.board[0][2] == self.board[1][1] == self.board[2][0]):
+        
+             return True
+               
+        return False
+   
+    
+    def is_tie(self):
+        for row in self.board:
+            if " " in row:
+                return False
+        return True
+            
+            
+    def change_player(self):
+        if self.current_player == "X":
+            self.current_player = "O" 
+        elif self.current_player == "O":
+            self.current_player = "X"
+        
+    
+               
+               
+       # DISLAY MESSAGES FOR A WIN OR TIED GAME
+       
+       
+    def display_winner(self):
+        messagebox.showinfo("Tic Tac Toe", f"Player {self.current_player} is the winner!")
+           
+           
+    def display_tie(self):
+        messagebox.showinfo("Tic Tac Toe", "The game is tied!")
+           
+       
+           
+       
+    def game_reset(self):
+        self.current_player = "X"
+        self.board = [[" " for _ in range(3)] for _ in range(3)]
+        for i in range(3):
+            for j in range(3):
+                self.buttons [i][j]["text"] = " "
+        
+    
+        
+
+
+if __name__ == "__main__":
+    game = TicTacToe()
+    game.run()
+
+#game = TicTacToe()
+#game.run()
